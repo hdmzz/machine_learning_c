c9cec58336a565d4bb01a9c71d2f779562589e1c
hugo <hdamitzian@gmail.com>
2025-01-02 10:29:10 +0100

    perdu dans la video, prochaine etape debut du model XOR

--- src/main.c
+++ src/main.c
@@ -23,24 +23,33 @@ float	cost( float w )
 		float	x = train[i][0];
 		float	y = x * w;
 		float	d = y - train[i][1];
-		result += d*d;
-		printf("expected %f, results %f\n", train[i][1], y);
+		result += d * d;
 	};
 	result /= train_count;
 
 	return ( result );
 };
 //prochaien etape peut etre faire un algoritme qui trouve la derivative d la fonction cost pour aller dans le sens inverse et se rapprocher de 0
-//nerd shit 
-// matrice et derivative en meme temps voila le proleme 
-//questce qune dderivative 
+//nerd shit
+// matrice et derivative en meme temps voila le proleme
+//questce qune dderivative
 int	main( void )
 {
 	srand( 69 );
 	float	w = rand_float() * 10.0f;
-	float	eps = 1e-3f;
-	printf("average error : %f\n", cost( w ));
-	printf("average error epsilon : %f\n", cost( w - eps ));
-	printf("average error epsilon : %f\n", cost( w - eps*2 ));
+	float	eps = 1e-3;
+	//on va trouve la finite difference, une aproximation de la derivee, qu'il faudra soustraire au w pourquoi? pour aller dans le sens inverse
+	float	learing_rate = 1e-3;
+
+	for ( size_t i = 0; i < 2000; i++ ) {
+		//approximation de la dérivée de la fonction cost 
+		float	dcost = ( cost( w + eps ) - cost( w )) / eps;
+		//soustraire la difference finie de la fonction cost au poids pour aller dans le sens inverse et donc se rapprocher de 0
+		w -= dcost * learing_rate;
+		printf( "average error : %f\n", cost( w ));
+	};
+	printf( "--------------------------------------\n" );
+	printf( "%f\n", w );
+
 	return ( 0 );
 };
